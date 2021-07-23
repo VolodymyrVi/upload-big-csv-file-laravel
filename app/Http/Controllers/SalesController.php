b574da5c43a982e57f6417441ac9637a83497843
VolodymyrVi <sqlphpjs@gmail.com>
2021-07-23 16:00:41 +0300

    + method store

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -17,8 +17,8 @@ class SalesController extends Controller
         if (request()->has('mycsv')) {
             //$data = array_map('str_getcsv', file(request()->mycsv));
             $data = file(request()->mycsv);
-            $header = $data[0];
-            unset($data[0]);
+            //$header = $data[0];
+            //unset($data[0]);
 
             // Chunking file
             $chunks = array_chunk($data, 1000);
@@ -43,5 +43,26 @@ class SalesController extends Controller
         return "please upload file";
     }
 
-    
+    public function store()
+    {
+        $path = resource_path('temp');
+        $files = glob("$path/*.csv");
+
+        $header = [];
+
+        foreach ($files as $key => $file) {
+            $data = array_map('str_getcsv', file($file));
+            if ($key == 0) {
+                $header = $data[0];
+                unset($data[0]); 
+            }
+
+
+            foreach ($data as $sale) {
+                $saleData = array_combine($header, $sale);
+                Sales::create($saleData);
+            }
+        }
+        return "stored"; 
+    }
 }
